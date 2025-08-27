d1a34e7fd5acf768e3c612b5463989d99e30a7a3
hilmyhafizhhh <hafizhhilmy17@gmail.com>
2025-08-27 21:10:23 +0700

    membuat Home Page

--- js/script.js
+++ js/script.js
@@ -5,4 +5,19 @@ const toggleNav = () => {
   navbar.classList.toggle("active");
   mobileNav.classList.toggle("hamburger-active");
 };
+
 mobileNav.addEventListener("click", () => toggleNav());
+
+document.addEventListener("click", (e) => {
+  if (navbar.classList.contains("active")) {
+    if (!navbar.contains(e.target) && !mobileNav.contains(e.target)) {
+      navbar.classList.remove("active");
+      mobileNav.classList.remove("hamburger-active");
+    }
+  }
+});
+
+const username = document.querySelector("#username");
+
+let name = prompt("Masukkan nama kamu:") || "Guest";
+username.textContent = name;
